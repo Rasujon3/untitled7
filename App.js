7accff3048fa4e4afbab5b937a7ae3e0dd096834
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-28 04:51:33 +0600

    Date Picker
    Link : https://documentation.onesignal.com/docs/react-native-sdk-setup
    
    CMNT : npm install --save react-native-onesignal
    
    react-native link react-native-onesignal

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ class App extends Component {
     render() {
         return (
             <View>
-              <Text>Push Notification</Text>
+                <Text>Date Picker</Text>
             </View>
         );
     }
